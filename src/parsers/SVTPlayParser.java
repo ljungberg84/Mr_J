31c9359de3720dd6295082a45ae576522efdbf15
Martin <westerlund_martin@hotmail.com>
2018-12-11 16:34:18 +0100

    add SvtPlay parser

---
+++ src/parsers/SVTPlayParser.java
@@ -0,0 +1,48 @@
+package parsers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class SVTPlayParser extends ServiceParser{
+
+    public SVTPlayParser() {
+        super("https://www.svtplay.se");
+    }
+
+    @Override
+    protected MovieInfo runScript(String movieTitle) {
+        browser.get(rootUrl);
+        try{
+            WebElement searchField = new WebDriverWait(browser, 5).
+                    until(ExpectedConditions.presenceOfElementLocated(By.id("q")));
+            searchField.sendKeys(movieTitle);
+
+            WebElement searchTab = new WebDriverWait(browser, 5).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@class='play_search__submit lp_button']")));
+            searchTab.click();
+            WebElement movieElement = new WebDriverWait(browser, 5).until(ExpectedConditions.presenceOfElementLocated
+                    (By.xpath("//a//span[@class='play_search-suggestions__link-text play_js-search-suggestion-title'][contains(translate(text(), "+
+                            "'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" + movieTitle.toLowerCase() + "')]")));
+
+
+
+            WebElement urlElement = new WebDriverWait(browser, 5).until(ExpectedConditions.presenceOfElementLocated
+                    (By.xpath("//a[@class='play_search-result__link']")));
+
+            String url = urlElement.getAttribute("href");
+
+            return new MovieInfo(movieTitle, url, "SVT Play");
+
+        }catch(TimeoutException e){
+            System.out.println(movieTitle + " was not found on SVTPlay");
+            //e.printStackTrace();
+
+        }finally {
+            browser.close();
+        }
+        return  null;
+    }
+}
