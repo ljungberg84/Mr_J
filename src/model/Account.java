afd31487569802cc3d9169f43a61d8bee06231e5
Martin <westerlund_martin@hotmail.com>
2018-12-13 12:46:35 +0100

    Add Account class and HBOAccount
    Waiting for script

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,4 +1,38 @@
 package model;
 
-public class Account {
+import parsers.MovieInfo;
+import parsers.ServiceParser;
+
+import java.util.HashMap;
+
+public abstract class Account extends ServiceParser {
+
+    private String userName;
+    private String passWord;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public Account(String rootUrl, String userName, String passWord) {
+        super(rootUrl);
+        this.userName = userName;
+        this.passWord = passWord;
+
+    }
+
+    public abstract void Login();
+
 }
