2c88076c3923f65c610b5179d31dcfa13e97fdaf
Bartek Kozuszek <bartekkozuszek@gmail.com>
2018-12-14 00:09:00 +0100

    Updated viaplay and hbo, deleted my user info. Problem with viaplay cookies doesnt keep you logged in

--- src/FX/Controller.java
+++ src/FX/Controller.java
@@ -30,34 +30,23 @@ public class Controller {
         ServiceParser netflixParser = new NetflixParser();
         ServiceParser viaplayParser = new ViaplayParser();
         //ServiceParser showtime = new ShowtimeParser();
+        ServiceParser svtParser = new SVTPlayParser();
 
         p.addService("hbo", hboParser);
         p.addService("netflix", netflixParser);
+        p.addService("Svt play", svtParser);
         p.addService("viaplay", viaplayParser);
         //program.addService(showtime);
-        //program.start();
+        //program.startSearch();
         listview.setItems(p.getHits());
-        p.getHits().addListener(new ListChangeListener<MovieInfo>() {
-            @Override
-            public void onChanged(Change<? extends MovieInfo> c) {
-                listview.refresh();
-            }
-        });
-    }
-
-    @FXML
-    public void buttonOnAction(){
-        try {button.setOnAction(event -> printText()
-        );
-        }catch (NullPointerException n){
-            System.out.println("Error");
-        }
+        p.startLogin();
+        p.getHits().addListener((ListChangeListener<MovieInfo>) c -> listview.refresh());
     }
 
 
     @FXML
     public void search(){
-        Thread searchThread = new Thread(()->p.start(search.getText()));
+        Thread searchThread = new Thread(()->p.startSearch(search.getText()));
         searchThread.start();
 
     }
--- src/model/Account.java
+++ src/model/Account.java
@@ -1,8 +1,34 @@
 package model;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import parsers.MovieInfo;
 import parsers.ServiceParser;
 
-public abstract class Account {
+import java.io.File;
+import java.util.List;
 
+public class Account {
+
+    private String userName;
+    private String password;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
