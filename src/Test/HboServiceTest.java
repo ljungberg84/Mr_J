5151b9811ef9695c3e3baa68ab55de238dd66d51
Petros Tsavdaridis <peteropon@gmail.com>
2018-12-17 16:59:07 +0100

    Add assertions

--- src/Test/HboServiceTest.java
+++ src/Test/HboServiceTest.java
@@ -8,17 +8,19 @@ import static org.junit.Assert.*;
 
 public class HboServiceTest {
 
-    private MovieInfo MovieInfo = new MovieInfo("Vikings",
+    private MovieInfo vikings = new MovieInfo("Vikings",
             "https://se.hbonordic.com/series/vikings/99bee710-f406-4a98-a319-f929eedbdabf",
             "https://static.hbonordic.com/categoryThumbnails/b6aa58b9-3189-44c0-813a-5f4c2c60a562.jpg", "Hbo.jpg");
 
     @Test
-    public void searchReturnsResult() {
+    public void searchReturnsCorrectResults() {
         HboService hbo = new HboService();
         MovieInfo object = hbo.search("vikings");
 
-        assertEquals("Returns wrong source", MovieInfo.getSource(), "Hbo.jpg");
-        assertEquals("Returns wrong title", MovieInfo.getTitle(), "Vikings");
+        assertEquals("Returns wrong source", object.getSource(), vikings.getSource());
+        assertEquals("Returns wrong title", object.getTitle(), vikings.getTitle());
+        assertEquals("Wrong url", object.getUrl(), vikings.getUrl());
+        assertEquals("Wrong image path", object.getImagePath(), vikings.getImagePath());
     }
 
     @Test
