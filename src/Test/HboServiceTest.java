ef61751c703e06781624e6b23a9b28d6ed9716bd
Petros Tsavdaridis <peteropon@gmail.com>
2018-12-17 16:42:31 +0100

    Write test for search method (HBO)

--- src/Test/HboServiceTest.java
+++ src/Test/HboServiceTest.java
@@ -8,14 +8,17 @@ import static org.junit.Assert.*;
 
 public class HboServiceTest {
 
-    private Object MovieInfo;
+    private MovieInfo MovieInfo = new MovieInfo("Vikings",
+            "https://se.hbonordic.com/series/vikings/99bee710-f406-4a98-a319-f929eedbdabf",
+            "https://static.hbonordic.com/categoryThumbnails/b6aa58b9-3189-44c0-813a-5f4c2c60a562.jpg", "Hbo.jpg");
 
     @Test
     public void searchReturnsResult() {
         HboService hbo = new HboService();
         MovieInfo object = hbo.search("vikings");
 
-        assertEquals(MovieInfo, object);
+        assertEquals("Returns wrong source", MovieInfo.getSource(), "Hbo.jpg");
+        assertEquals("Returns wrong title", MovieInfo.getTitle(), "Vikings");
     }
 
     @Test
