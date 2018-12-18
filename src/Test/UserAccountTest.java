b4f9b547c3b5f18ffe60e8058a7619196c0c789f
Martin <westerlund_martin@hotmail.com>
2018-12-18 19:48:34 +0100

    Add UserAccountTest class

---
+++ src/Test/UserAccountTest.java
@@ -0,0 +1,37 @@
+import model.UserAccount;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserAccountTest {
+
+    private UserAccount account = new UserAccount("kalle", "hemligt");
+
+    @Test
+    public void hasLogin() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void getUserName() {
+        assertEquals(account.getUserName(), "kalle");
+    }
+
+    @Test
+    public void setUserName() {
+        account.setUserName("Olle");
+        assertEquals(account.getUserName(), "Olle");
+    }
+
+    @Test
+    public void getPassword() {
+
+        assertEquals(account.getPassword(), "hemligt");
+    }
+
+    @Test
+    public void setPassword() {
+        account.setPassword("vanligt");
+        assertEquals(account.getPassword(), "vanligt");
+    }
+}
